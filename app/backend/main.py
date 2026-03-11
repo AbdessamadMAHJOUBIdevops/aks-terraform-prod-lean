1f113c35a8d119efd7d35089055af987bd27206b
Abdessamad Mahjoubi <abdessamad.mahjoubi@etixway.com>
2026-03-11 12:41:27 +0100

    feat: comportement SRE

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,31 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from prometheus_fastapi_instrumentator import Instrumentator
 import redis
 import os
+import time
+import random
 
 app = FastAPI()
 
-# On récupère l'adresse de Redis via les variables d'environnement
-# Si K8s ne nous donne rien on essaie l'adresse par défaut "redis-service"
 REDIS_HOST = os.getenv("REDIS_HOST", "redis-service")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-
-# Connexion à la base de données
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+
+#  MAGIE SRE : Cette ligne instrumente toute l'API automatiquement
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
     try:
-        # On incrémente un compteur de visites dans Redis à chaque appel
         visits = r.incr("visitor_count")
-        return {
-            "status": "success",
-            "message": "Bonjour depuis l'API FastAPI K8s !",
-            "visits": visits
-        }
-    except redis.exceptions.ConnectionError:
-        # Si Redis n'est pas encore déployé, on renvoie quand même une réponse
-        return {
-            "status": "warning",
-            "message": "L'API tourne, mais la base de données Redis est introuvable.",
-            "visits": "Inconnu"
-        }
+        return {"status": "success", "message": "API K8s", "visits": visits}
+    except Exception:
+        return {"status": "warning", "message": "Redis introuvable"}
+
+#  ROUTE DU CHAOS : Pour tester nos alertes SRE !
+@app.get("/chaos")
+def make_chaos():
+    chance = random.random()
+    
+    # 20% de chances de crasher violemment (Erreur 500)
+    if chance < 0.2:
+        raise HTTPException(status_code=500, detail="Aïe ! Le serveur a explosé (Test SRE)")
+    
+    # 30% de chances de ramer énormément (Latence élevée)
+    elif chance < 0.5:
+        time.sleep(random.uniform(1.0, 3.0)) # Met l'API en pause entre 1 et 3 secondes
+        return {"status": "slow", "message": "C'était looooooong..."}
+    
+    # 50% de chances que tout aille bien
+    else:
+        return {"status": "ok", "message": "Coup de chance, ça marche !"}
+
+#  ROUTE DE STRESS CPU
+@app.get("/stress")
+def stress_cpu():
+    # Calcul inutile pour faire chauffer le processeur
+    x = 0
+    for i in range(5000000):
+        x += i
+    return {"message": "Le CPU a bien transpiré !"}
